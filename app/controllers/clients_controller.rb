2117fb9e4622128591d384f00009d14b7afe5a08
M.T. Davis <marquese.davis@gmail.com>
2016-06-05 00:11:10 +0000

    updated user profile page and added uniq order listing for client in controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,7 +6,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.all
+    @clients = Client.all.uniq.order("created_at DESC")
   end
 
   # GET /clients/1
