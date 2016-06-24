6d413999b0a2a076fb7ad7e4897e9cb4abe6081d
M.T. Davis <marquese.davis@gmail.com>
2016-06-24 02:21:29 +0000

    created migreation to fix heroku push

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624020240) do
+ActiveRecord::Schema.define(version: 20160624021808) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "firstName"
