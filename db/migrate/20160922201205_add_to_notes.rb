0539ad8ca63b384a0f1149f6ec542641881b7a34
Marquese Davis <marquese.davis@gmail.com>
2016-09-23 16:12:38 +0000

    fixed migration

--- db/migrate/20160922201205_add_to_notes.rb
+++ db/migrate/20160922201205_add_to_notes.rb
@@ -1,6 +1,6 @@
 class AddToNotes < ActiveRecord::Migration
   def change
     #add_column :notes, :user_id, :integer
-    add_column :notes, :client_id, :integer
+    #add_column :notes, :client_id, :integer
   end
 end
