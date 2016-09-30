ea83f934228e29a683170ec6e790dff8b0a015ae
Marquese Davis <marquese.davis@gmail.com>
2016-09-30 15:06:59 +0000

    removed links in header and comment out route for devise sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-  end
+  #devise_scope :user do
+  #  get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+  #  get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  #end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :otcs
@@ -26,13 +26,13 @@ Rails.application.routes.draw do
   resources :rsources
   resources :states
   resources :notes
-  #devise_for :users
+  devise_for :users
   #devise_for :users, :skip => :registrations
-  devise_for :users, :skip => [:registrations] 
-  as :user do
-  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-  put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end
+  #devise_for :users, :skip => [:registrations] 
+  #as :user do
+  #get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  #put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  #end
   #devise_for :users, :skip => [:registrations]
   #as :user do
   #  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
